d9a1191e2ad4e9409bf169cff2fc3fc81058de20
TheEntropyShard <theentropyshard@gmail.com>
2026-02-14 14:26:05 +0300

    Images are centered now

--- composeApp/src/jvmMain/kotlin/me/theentropyshard/elysme/ui/chat/ChatMessage.kt
+++ composeApp/src/jvmMain/kotlin/me/theentropyshard/elysme/ui/chat/ChatMessage.kt
@@ -218,7 +218,7 @@ fun ChatMessage(
                                 val newHeight = (message.dimensionsHeight * scale).dp
 
                                 ImageAttachment(
-                                    modifier = Modifier.size(newWidth, newHeight),
+                                    modifier = Modifier.size(newWidth, newHeight).align(Alignment.CenterHorizontally),
                                     scale = max(scale, 1.0f),
                                     name = message.fileName,
                                     filePath = message.file,
