4bfd6a521625a123bbcfc4c5773bd6d1f5d73448
TheEntropyShard <theentropyshard@gmail.com>
2026-02-15 13:28:54 +0300

    Request focus when clicking Reply

--- composeApp/src/jvmMain/kotlin/me/theentropyshard/elysme/ui/chat/ChatInput.kt
+++ composeApp/src/jvmMain/kotlin/me/theentropyshard/elysme/ui/chat/ChatInput.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.text.input.delete
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -64,6 +65,12 @@ fun ChatInput(
 
         state.edit { delete(start = 0, end = length) }
         requester.requestFocus()
+    }
+
+    LaunchedEffect(model.currentReplyTo) {
+        if (model.currentReplyTo != null) {
+            requester.requestFocus()
+        }
     }
 
     ChatInputBase(
